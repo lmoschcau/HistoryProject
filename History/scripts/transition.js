1ecf609a179d5b3ea1334595b4b8dbdc04b60fae
Leonard Moschcau <l.moschcau@web.de>
2018-01-14 20:29:06 +0100

    basic transition functionality added

--- History/scripts/transition.js
+++ History/scripts/transition.js
@@ -1,17 +1,42 @@
-function Vector(x, y) {
-    this.x = x || 0;
-    this.y = y || 0;
+var transitions = {
+    list: [],
+    addTransition: function (object, cssStyle, unit, transitionStartScroll, transitionEndScroll, start, end, easing) {
+        this.list.push(new transition(object, cssStyle, unit, transitionStartScroll, transitionEndScroll, start, end, easing));
+    },
+    addTransitionsAutomatic: function () {
+        var objects = $("[data-transition]");
+        console.log(objects);
+    }
+};
+
+function transition(object, cssStyle, unit, transitionStartScroll, transitionEndScroll, start, end, easing) {
+    this.object = object;
+    this.cssStyle = cssStyle;
+    this.unit = unit.split("$V") || $Vpx;
+    this.transitionStartScroll = transitionStartScroll;
+    this.transitionEndScroll = transitionEndScroll;
+    this.start = start;
+    this.end = end;
+    this.easing = easing;
+}
+transition.prototype.isInRange = function (scroll) {
+    return ((this.transitionStartScroll < scroll) && (scroll < this.transitionEndScroll));
 }
 
-var transitions = [];
-
-function addTransitionObject(object, transitionStartScroll, transitionEndScroll, startVector, endVector, easing) {
-    var transitionObject = { object, transitionStartScroll, transitionEndScroll, startVector, endVector, easing }
-    transitions.push(transitionObject);
+transition.prototype.updateTransition = function () {
+    easingFunction = $.easing[this.easing];
+    if (typeof easingFunction === "function") {
+        parameter = [0, ($(document).scrollTop() - this.transitionStartScroll), this.start, (this.end - this.start), (this.transitionEndScroll - this.transitionStartScroll)];
+        this.object.style[this.cssStyle] = (this.unit[0] + easingFunction.apply(null, parameter) + this.unit[1]);
+    } else { console.error("'" + this.easing + "' is not an easing function") }
 }
 
 function update() {
-    for (i = 0; i < transitions.length; i++) {
-        if ()
+    for (i = 0; i < transitions.list.length; i++) {
+        if (transitions.list[i].isInRange($(document).scrollTop())) {
+            transitions.list[i].updateTransition();
+        }
     }
+    requestAnimationFrame(update);
 }
+$(update());
