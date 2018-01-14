bdd83d6d6cc7ae55d4d929607a67f5cfc5645629
Leonard Moschcau <l.moschcau@web.de>
2018-01-14 15:10:05 +0100

    basic javascript functions added

--- History/scripts/transition.js
+++ History/scripts/transition.js
@@ -1,1 +1,17 @@
-// JavaScript source code
+function Vector(x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+}
+
+var transitions = [];
+
+function addTransitionObject(object, transitionStartScroll, transitionEndScroll, startVector, endVector, easing) {
+    var transitionObject = { object, transitionStartScroll, transitionEndScroll, startVector, endVector, easing }
+    transitions.push(transitionObject);
+}
+
+function update() {
+    for (i = 0; i < transitions.length; i++) {
+        if ()
+    }
+}
