a9d190e3d7f888343e9163b4719e90bb1f2e5ae7
Myzhe <33021852+Myzhe@users.noreply.github.com>
2018-01-17 17:12:56 +0100

    Update circle.js

--- History/scripts/circle.js
+++ History/scripts/circle.js
@@ -13,43 +13,58 @@ function fillDescription(state) {
   var info = [
     [
       "Dänemark",
-      "Dänemark ist muy bien."
+      "Dänemark ist muy bien.",
+      "https://i.imgur.com/tM1qHOF.png"
     ],
     [
       "Großbritannien",
-      "Dänemark ist muy bien."
+      "Dänemark ist muy bien.",
+      "https://i.imgur.com/MlYmMbt.png"
     ],
     [
       "Belgien",
-      "Dänemark ist muy bien."
+      "Dänemark ist muy bien.",
+      "https://i.imgur.com/PN5Nx87.png"
     ],
     [
       "Frankreich",
-      "Dänemark ist muy bien."
+      "Der deutsche Sieg über Frankreich von 1871 prägte die Beziehung der beiden Länder über Dekaden. Die Franzosen waren nicht bloß aufgrund der militärischen Niederlage, Annexion von Elsass-Lothringen oder Reparationen in Höhe von 5 Mrd. Francs, sondern der Proklamation des deutschen Kaisers inmitten Frankreichs - in Versailles - wegen erbost.<br>Ein neuer Meilenstein in der deutsch-französischen Erbfeindschaft wurde erreicht.<br><b class='hashtag'>bringBackElsassLothringen</b>",
+      "https://i.imgur.com/bGlIKFr.png"
     ],
     [
       "USA",
-      "Dänemark ist muy bien."
+      "Dänemark ist muy bien.",
+      "https://i.imgur.com/GvPjJXO.png"
     ],
     [
       "Spanien",
-      "Dänemark ist muy bien."
+      "Dänemark ist muy bien.",
+      "https://i.imgur.com/FJxMxav.png"
     ],
     [
       "Italien",
-      "Dänemark ist muy bien."
+      "Dänemark ist muy bien.",
+      "https://i.imgur.com/Q7H7GFv.png"
+    ],
+    [
+      "Osmanisches Reich",
+      "Bereits kurz nach der Gründung des Deutschen Reiches wandte sich Sultan Abdülhamid II an Bismarck. Er sah sein Reich durch die Kolonialbestrebungen Frankreichs, Englands, Russlands und Italiens im Orient bedroht.<br>Erste Kooperationen entstanden in militärischen Bereichen. Der Sultan beauftragte deutsche Firmen, seine desolate Armee durch deutsche Waffentechnologie auf Vordermann zu bringen.<br>Der wirtschaftliche Einfluss breitete sich so stark aus, dass deutsche Unternehmen auf dem türkischen Markt Monopolstellungen erreichen konnten. Außerdem realisierten diese Unternehmen die bedeutendsten türkischen Projekte dieser Zeit, wie den Bau der Bagdadbahn oder den der Anatolischen Bahn.<br><b class='hashtag'>Wirtschaftsmonopol</b>",
+      "https://i.imgur.com/dZjeHop.png"
     ],
     [
       "Österreich-Ungarn",
-      "Dänemark ist muy bien."
+      "Dänemark ist muy bien.",
+      "https://i.imgur.com/GreMppX.png"
     ],
     [
       "Japan",
-      "Dänemark ist muy bien."
+      "Japans Beziehung mit dem Deutschen Reich wurde seit der Reichsgründung gut gepflegt. Nach dem ersten Chinesisch-Japanischen Krieg kühlten die Beziehungen jedoch ab. Kohlelieferungen nach Russland während des Russisch-Japanischen Krieges, sowie der in Europa und Amerika populären Rassismus gegenüber fernöstlichen Ethnien in Form der <b class='hashtag'>gelben Gefahr</b> belasteten die Verbindung weiter.",
+      "https://i.imgur.com/XSmYsVT.png"
     ],
     [
       "Russland",
-      "Dänemark ist muy bien."
+      "Bismarcks Bündisse mit Russland durch den Dreikaiserbund und Rückversicherzungsvertrag wurden durch Wilhelm II. nicht mehr erneuert. Auf der Suche nach einem neuen Bündnispartner näherte sich das Zarenreich schließlich Frankreich und Großbritannien an. Der frühere Bündnispartner war ab 1892 nun mit Frankreich verbündet.",
+      "https://i.imgur.com/hFb3vxA.png"
     ]
   ];
 
@@ -63,4 +78,5 @@ function fillDescription(state) {
     elms[i].style.borderColor = "#f1f1f1";
   }
   document.getElementsByClassName('foreign')[state].style.borderColor = "#f00";
+  document.getElementsByClassName('foreign')[state].style.backgroundImage = "url('" + info[state][2] + "')";
 }
