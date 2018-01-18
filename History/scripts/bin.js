790e5505e053fb572b51717438eb6c7cce0f8055
Leonard Moschcau <35405331+MoschcauL@users.noreply.github.com>
2018-01-18 20:15:18 +0100

    Update bin.js

--- History/scripts/bin.js
+++ History/scripts/bin.js
@@ -12,11 +12,14 @@ function calcFakePages() {
 }
 
 function showSource(obj) {
-    $("#sourcePanel").html(obj.getAttribute("data-source"));
-    $("#sourcePanel")[0].style.top = $(obj).offset().top - $(window).scrollTop() + "px";
-    $("#sourcePanel")[0].style.left = (($(obj).offset().left - $(window).scrollLeft()) + ($(obj).width() / 2) + "px");
-    $("#sourcePanel")[0].style.display = "block";
-    toggleOverlay(true);
+    var source = obj.getAttribute("data-source");
+    if (source != "") {
+        $("#sourcePanel").html(source);
+        $("#sourcePanel")[0].style.top = $(obj).offset().top - $(window).scrollTop() + "px";
+        $("#sourcePanel")[0].style.left = (($(obj).offset().left - $(window).scrollLeft()) + ($(obj).width() / 2) + "px");
+        $("#sourcePanel")[0].style.display = "block";
+        toggleOverlay(true);
+    }
 }
 
 function toggleOverlay(state) {
