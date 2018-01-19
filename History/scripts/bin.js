3406e12f82dfe0975708bc3c5ee2d4d615ff2d25
Leonard Moschcau <l.moschcau@web.de>
2018-01-19 19:38:36 +0100

    comments added in bin.js and transition.js

--- History/scripts/bin.js
+++ History/scripts/bin.js
@@ -1,41 +1,41 @@
-function initBin() {
-    calcFakePages();
-    st_build();
-    $("[data-source]").click(function () { showSource(this) });
+function initBin() { // initalize combined function
+    calcFakePages(); // call calcFakePages()
+    st_build(); // call st_build() inside circle.js
+    $("[data-source]").click(function () { showSource(this) }); // apply click event to all source elements (Quellen)
 }
 
-$(initBin);
+$(initBin); // call init() once dom is ready to be manipulated
 
-function calcFakePages() {
-    var length = $(".page").length * $(window).height() * 2;
-    $("#fakeContent").css("height", (length + "px"));
+function calcFakePages() { // calc fake content height because the page needs to bee scrollable
+    var length = $(".page").length * $(window).height() * scrollFactor; // get number of pages, multiply by page hight and scrollFactor
+    $("#fakeContent").css("height", (length + "px")); // set height of fake content
 }
 
-function showSource(obj) {
-    var source = obj.getAttribute("data-source");
-    if (source.length >= 0) {
-        $("#sourcePanel").html(source);
-        $("#sourcePanel")[0].style.top = $(obj).offset().top - $(window).scrollTop() + "px";
-        $("#sourcePanel")[0].style.left = (($(obj).offset().left - $(window).scrollLeft()) + ($(obj).width() / 2) + "px");
-        $("#sourcePanel")[0].style.display = "block";
-        toggleOverlay(true);
+function showSource(obj) { // show source by object
+    var source = obj.getAttribute("data-source"); // get the source text
+    if (source.length >= 0) { // check if the source text is not empty
+        $("#sourcePanel").html(source); // set HTML of the soucePanel object to the souce text
+        $("#sourcePanel")[0].style.top = $(obj).offset().top - $(window).scrollTop() + "px"; // get the source objects offset and the global scrollValue and calculate the top offset
+        $("#sourcePanel")[0].style.left = (($(obj).offset().left - $(window).scrollLeft()) + ($(obj).width() / 2) + "px"); // same procedure as with top offset but add 1/2 of the source objects width
+        $("#sourcePanel")[0].style.display = "block"; // set the display from none to block
+        toggleOverlay(true); // toggle the overlay to true
     }
 }
 
-function toggleOverlay(state) {
-    if (state) {
-        $("#overlay")[0].style.display = "block";
+function toggleOverlay(state) { //toggle overlay to state
+    if (state) { // if overlay should be visible
+        $("#overlay")[0].style.display = "block"; // set display from none to block
     } else {
-        $("#overlay")[0].style.display = "none";
-        $("#sourcePanel")[0].style.display = "none";
+        $("#overlay")[0].style.display = "none"; // set display value of overlay to none
+        $("#sourcePanel")[0].style.display = "none"; // set display value of sourcePanel to none
     }
 }
 
 function slideShow() {
-    $('html, body').animate({ scrollTop: 0 }, 3000);
-    $('html, body').animate({ scrollTop: $("#fakeContent").height() }, 60000);
+    $('html, body').animate({ scrollTop: 0 }, 3000); // scroll to the top of the page (in 3000ms(=3s))
+    $('html, body').animate({ scrollTop: $("#fakeContent").height() }, 60000); // scroll to the bottom (fakeContent height) of the page (in 60000ms(=60s))
 }
 
-function pageScroll(destination) {
-    $('html, body').animate({ scrollTop: (destination * $(window).height() * 2) + 200 }, 3000);
+function pageScroll(destination) { // scroll to destination
+    $('html, body').animate({ scrollTop: (destination * $(window).height() * scrollFactor) + 200 }, 3000); // scroll to destination (page number) * window height * scrollFactor + 200 (fix) (in 3000ms(=3s))
 };
--- History/scripts/transition.js
+++ History/scripts/transition.js
@@ -1,89 +1,97 @@
+scrollFactor = 2; // devide speed by Factor
+/*
+all transitions. methods deal with the administration of transitions
+
+all transition. methods deal with the manipulation of HTML elements trough transitions
+
+ */
+
+
 var transitions = {
-    list: [],
-    addTransition: function (object, cssStyle, unit, transitionStartScroll, transitionEndScroll, start, end, easing) {
+    list: [], // creates array of all transition objects
+    addTransition: function (object, cssStyle, unit, transitionStartScroll, transitionEndScroll, start, end, easing) { // function to add a transition to the array
         this.list.push(new transition(object, cssStyle, unit, transitionStartScroll, transitionEndScroll, start, end, easing));
     },
-    addTransitionsAutomatic: function () {
-        var objects = $("[data-transition]");
-        function returnOne(first, second) {
-            var first = (typeof first !== 'undefined') ? first : second;
+    addTransitionsAutomatic: function () { // function designed to call  addTransition automatic on every element with data-transition set
+        var objects = $("[data-transition]"); // put all elements with data-transition set into objects
+        function returnOne(first, second) { // shorthand version 
+            var first = (typeof first !== 'undefined') ? first : second; // return one of the two
             return first;
         }
-        for (var i = 0; i < objects.length; i++) {
-            var transitionsJSON = JSON.parse(objects[i].getAttribute("data-transition").replace(/["']/g, '"'));
-            for (var c = 0; c < transitionsJSON.transitions.length; c++) {
-                var cu = transitionsJSON.transitions[c];
-                transitions.addTransition(objects[i], returnOne(cu.c, cu.css), returnOne(cu.u, cu.unit), returnOne(cu.sY, cu.startY), returnOne(cu.eY, cu.endY), returnOne(cu.sV, cu.startValue), returnOne(cu.eV, cu.endValue), returnOne(cu.e, cu.easing));
+        for (var i = 0; i < objects.length; i++) { //iterate trough all objects 
+            var transitionsJSON = JSON.parse(objects[i].getAttribute("data-transition").replace(/["']/g, '"')); // get the JSON data AND replace the ' with " AND parse to javascript object
+            for (var c = 0; c < transitionsJSON.transitions.length; c++) { // iterate trough javascript object
+                var cu = transitionsJSON.transitions[c]; // set current
+                transitions.addTransition(objects[i], returnOne(cu.c, cu.css), returnOne(cu.u, cu.unit), returnOne(cu.sY, cu.startY), returnOne(cu.eY, cu.endY), returnOne(cu.sV, cu.startValue), returnOne(cu.eV, cu.endValue), returnOne(cu.e, cu.easing)); //call transitions.addTransition() to add transition
             }
         }
-        console.log(objects);
     }
 };
-function transition(object, cssStyle, unit, transitionStartScroll, transitionEndScroll, start, end, easing) {
+function transition(object, cssStyle, unit, transitionStartScroll, transitionEndScroll, start, end, easing) { // define transition object
     this.object = object;
     this.cssStyle = cssStyle;
-    this.unit = unit.split("$V") || $Vpx;
+    this.unit = unit.split("$V") || $Vpx; // default
     this.transitionStartScroll = transitionStartScroll;
     this.transitionEndScroll = transitionEndScroll;
     this.start = start;
     this.end = end;
-    this.easing = easing;
+    this.easing = easing; // easing function from easing.js
 }
-transition.prototype.isInRange = function (scroll) {
+transition.prototype.isInRange = function (scroll) { // check if the specific transition has to get animated
     return ((this.absolute(this.transitionStartScroll) < scroll) && (scroll < this.absolute(this.transitionEndScroll)));
 }
 
-transition.prototype.absolute = function (value) {
-    return (value / (100/2) * $(window).height());
+transition.prototype.absolute = function (value) { // convert from % to px
+    return (value / (100 / scrollFactor) * $(window).height()); // also include scrollFactor
 }
 
-transition.prototype.updateTransition = function () {
-    var easingFunction = $.easing[this.easing];
-    if (typeof easingFunction === "function") {
-        this.object.style[this.cssStyle] = (this.unit[0] + easingFunction(0, ($(document).scrollTop() - this.absolute(this.transitionStartScroll)), this.start, (this.end - this.start), (this.absolute(this.transitionEndScroll) - this.absolute(this.transitionStartScroll))) + this.unit[1]);
-    } else { console.error("'" + this.easing + "' is not an easing function") }
+transition.prototype.updateTransition = function () { // apply style to HTML element
+    var easingFunction = $.easing[this.easing]; // get function witch the name of easingFunction
+    if (typeof easingFunction === "function") { // check if function exists
+        this.object.style[this.cssStyle] = (this.unit[0] + easingFunction(0, ($(document).scrollTop() - this.absolute(this.transitionStartScroll)), this.start, (this.end - this.start), (this.absolute(this.transitionEndScroll) - this.absolute(this.transitionStartScroll))) + this.unit[1]); // apply and combine value with unit
+    } else { console.error("'" + this.easing + "' is not an easing function") } // trow error if easing Function does not exists
 }
 
-function update() {
-    if (flag) {
-        for (var i = 0; i < transitions.list.length; i++) {
-            if (transitions.list[i].isInRange($(document).scrollTop())) {
-                transitions.list[i].updateTransition();
+function update() { // call updateTransition() on every transition in Range on requestAnimationFrame()
+    if (flag) { // check if scrolling
+        for (var i = 0; i < transitions.list.length; i++) { // iterate trough all transitions
+            if (transitions.list[i].isInRange($(document).scrollTop())) { // is transition in range
+                transitions.list[i].updateTransition(); // updateTransition()
             }
         }
     }
-    requestAnimationFrame(update);
+    requestAnimationFrame(update); // request new animation frame
 }
 
-function updateFix() {
-    scroll = $(document).scrollTop();
-    for (var i = 0; i < transitions.list.length; i++) {
-        var cur = transitions.list[i];
-        if (cur.absolute(cur.transitionStartScroll) > scroll) {
-            cur.object.style[cur.cssStyle] = (cur.unit[0] + cur.start + cur.unit[1]);
+function updateFix() { // fix elements wich are misplaced because of steps skipped while scrolling and are out of range
+    scroll = $(document).scrollTop(); // get global scroll
+    for (var i = 0; i < transitions.list.length; i++) { // iterate trough all transitions
+        var cur = transitions.list[i]; // set short variable
+        if (cur.absolute(cur.transitionStartScroll) > scroll) { // check if scroll is below transitionStartScroll
+            cur.object.style[cur.cssStyle] = (cur.unit[0] + cur.start + cur.unit[1]); // apply and combine value with unit
         }
-        if (cur.absolute(cur.transitionEndScroll) < scroll) {
-            cur.object.style[cur.cssStyle] = (cur.unit[0] + cur.end + cur.unit[1]);
+        if (cur.absolute(cur.transitionEndScroll) < scroll) { // check if scroll is above transitionEndScroll
+            cur.object.style[cur.cssStyle] = (cur.unit[0] + cur.end + cur.unit[1]); // apply and combine value with unit
 
         }
     }
 }
 
-function init() {
-    transitions.addTransitionsAutomatic();
-    update();
+function init() { // initalize combined function
+    transitions.addTransitionsAutomatic(); // initally add transitions automatic
+    update(); // start update() cicle
 }
-$(init);
+$(init); // call init() once dom is ready to be manipulated
 
-var timer, flag = false;
-$(window).scroll(function () {
-    if (!flag) {
+var timer, flag = false; // flag to check if user is scrolling at any point in the script
+$(window).scroll(function () { // check if user is scrolling
+    if (!flag) { //set flag true if user is scrolling
         flag = true;
     }
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-        flag = false;
-    }, 200);
+    clearTimeout(timer); // if user is scrolling clear previous timer
+    timer = setTimeout(function () { // set new timeout
+        flag = false; // callback function is used to reset flag
+    }, 200); // timeout after 200ms
 });
 
-setInterval(updateFix, 500);
+setInterval(updateFix, 500); // call updateFix every 500ms to resolve issues
